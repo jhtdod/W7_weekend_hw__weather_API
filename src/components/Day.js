444f37287ff47eaffef6f7f9d6ee4e10b2fc9b2c
jhtdod <jess.dodge@hotmail.co.uk>
2022-04-09 17:08:01 +0100

    react-bootstrap and bootstrap installed. Tabs added to display weather for individual days

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,20 +1,19 @@
 import React from "react";
 import '../static/DayComponent.css';
 import { CloudDrizzle, Sunrise, Sunset, Thermometer, Wind } from 'react-feather';
+import { Tab } from "bootstrap";
 
 const Day = ({ date, tempMax, tempMin, precip, sunrise, sunset, windspeed }) => {
 
-
     return (
         <div className="day">
-            <h3>{date}</h3>
             <ul>
-                <li><Thermometer size={18}/>Max {tempMax}&#176;C</li>
-                <li><Thermometer size={18}/>Min {tempMin}&#176;C</li>
-                <li><CloudDrizzle size={18}/> {precip} mm</li>
-                <li><Sunrise size={18}/> {sunrise.slice(11)}</li>
-                <li><Sunset size={18}/> {sunset.slice(11)}</li>
-                <li><Wind size={18}/> {windspeed} mph</li>
+                <li><Thermometer size={18} />Max {tempMax}&#176;C</li>
+                <li><Thermometer size={18} />Min {tempMin}&#176;C</li>
+                <li><CloudDrizzle size={18} /> {precip} mm</li>
+                <li><Sunrise size={18} /> {sunrise.slice(11)}</li>
+                <li><Sunset size={18} /> {sunset.slice(11)}</li>
+                <li><Wind size={18} /> {windspeed} mph</li>
             </ul>
         </div>
     )
--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -1,28 +1,47 @@
 import React from "react";
 import Day from "./Day";
+import "bootstrap/dist/css/bootstrap.min.css"
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+import { Nav, NavItem, NavLink, Row, TabContainer, TabContent, TabPane } from "react-bootstrap";
 
 const WeatherList = ({ days }) => {
 
     const dayNodes = days.map(day => {
         return (
-            <Day
-                date={day.date}
-                tempMax={day.tempMax}
-                tempMin={day.tempMin}
-                precip={day.precip}
-                sunrise={day.sunrise}
-                sunset={day.sunset}
-                windspeed={day.windspeed}
-                key={day.id}
-            />
+            <div>
+                <Nav variant="tabs">
+                    <NavItem>
+                        <NavLink eventKey={day.date}>{day.date}</NavLink>
+                    </NavItem>
+                </Nav>
+                <TabContent>
+                    <TabPane eventKey={day.date}>
+                        <Day
+                            date={day.date}
+                            tempMax={day.tempMax}
+                            tempMin={day.tempMin}
+                            precip={day.precip}
+                            sunrise={day.sunrise}
+                            sunset={day.sunset}
+                            windspeed={day.windspeed}
+                            key={day.id}
+                        />
+                    </TabPane>
+                </TabContent>
+            </div>
         )
+
 
     })
 
     return (
         <div>
             <div className="display-flex">
-                {dayNodes}
+                <TabContainer>
+                    {dayNodes}
+                </TabContainer>
+
             </div>
         </div>
     )
